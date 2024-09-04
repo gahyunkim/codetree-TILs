5b8ac9dc2d92ad587cde194475226931017e5d0a
gahyunkim <mapledt001@naver.com>
2024-09-04 14:53:05 +0900

    Title: 4번의 크기 비교, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240904/4번의 크기 비교/4-time-comparison.py
+++ 240904/4번의 크기 비교/4-time-comparison.py
@@ -2,22 +2,7 @@ a=int(input())
 arr=input().split()
 b,c,d,e = int(arr[0]),int(arr[1]),int(arr[2]),int(arr[3])
 
-if a>b:
-    print(1)
-else:
-    print(0)
-
-if a>c:
-    print(1)
-else:
-    print(0)
-
-if a>d:
-    print(1)
-else:
-    print(0)
-
-if a>e:
-    print(1)
-else:
-    print(0)
+print(int(a>b))
+print(int(a>c))
+print(int(a>d))
+print(int(a>e))
