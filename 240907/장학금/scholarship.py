5303601a017f95d4135945e3441ea24fada0be17
gahyunkim <mapledt001@naver.com>
2024-09-07 23:19:28 +0900

    Title: 장학금, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240907/장학금/scholarship.py
+++ 240907/장학금/scholarship.py
@@ -2,8 +2,8 @@ arr=input().split()
 a,b= int(arr[0]), int(arr[1])
 
 if a>=90 and f>=95:
-    print(10000)
+    print("10000")
 elif a>=90 and f>=90:
-    print(50000)
+    print("50000")
 else:
-    print(0)
+    print("0")
