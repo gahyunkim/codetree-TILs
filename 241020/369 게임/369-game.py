8feafa0b4cdbb97e5e1bd673001db5c3e4709a04
gahyunkim <mapledt001@naver.com>
2024-10-20 01:04:49 +0900

    Title: 369 게임, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 241020/369 게임/369-game.py
+++ 241020/369 게임/369-game.py
@@ -1,7 +1,12 @@
-n=int(input())
+n = int(input())
 
-for i in range(1,n+1):
-    if i%3==0 or i==6 or i==9 :
-        print(0, end=' ')
-    else:   
-        print(i, end=' ')
+
+for i in range(1, n+1):
+	if i % 3 == 0:
+		print("0", end=" ")
+	elif i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+		print("0", end=" ")
+	elif i // 10 == 3 or i // 10 == 6 or i // 10 == 9:
+		print("0", end=" ")
+	else:
+		print(i, end=" ")
